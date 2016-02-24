1bede501945e461d737a69335ded9cc657bf59a9
Alexis <BaxTer@PC>
2016-02-24 14:41:07 +0100

    ajout connection bdd + premiere fonction pour exemple

--- ECommerce/src/com/hitema/home/HomeController.java
+++ ECommerce/src/com/hitema/home/HomeController.java
@@ -1,8 +1,18 @@
 package com.hitema.home;
 
+import com.hitema.beans.ArticleBean;
+import com.hitema.db.DBCommerceController;
+
 public class HomeController {
 
+	DBCommerceController db = new DBCommerceController();
+	
 	public String execute(){
+		ArticleBean article;
+		db.init();
+		article = db.getArticleById("1");
+		db.close();
+		System.out.println(article.toString());
 		return "home";
 	}
 }
