ba85afc4e7f1da762e6022fa475d0a10b9af8ac1
AlexisGot <alexis.got.pro@gmail.com>
2016-02-25 14:49:40 +0100

    changes

--- ECommerce/src/com/hitema/db/DBCommerceController.java
+++ ECommerce/src/com/hitema/db/DBCommerceController.java
@@ -4,6 +4,11 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.apache.commons.logging.impl.Log4JLogger;
 
 import com.hitema.beans.ArticleBean;
 
@@ -24,9 +29,26 @@ public class DBCommerceController{
 			ps.setString(1, id);
 			ResultSet rs = ps.executeQuery();
 			article = DBUtils.getArticleFromResultSet(rs);
+			rs.close();
+			connection.close();
 		} catch (SQLException e) {
 		}
 		return article;
+	}
+	
+	public List<ArticleBean> getAllArticles(){
+		List<ArticleBean> articles = new ArrayList<ArticleBean>();
+		try {
+			String sql = "SELECT * FROM article";
+			Connection connection = db.getConnection();
+			PreparedStatement ps = connection.prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+			articles = DBUtils.getAllArticlesFromResultSet(rs);
+			rs.close();
+			connection.close();
+		} catch (SQLException e) {
+		}
+		return articles;
 	}
 	
 	public void close() {
