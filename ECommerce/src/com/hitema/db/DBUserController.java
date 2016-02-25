d23c24c0d37f6035dd4c3f97e5a1ae7aaafe8ac1
antoinehagues <antoine.hagues@gmail.com>
2016-02-25 13:49:08 +0100

    Debug des erreurs DBUserController.java

--- ECommerce/src/com/hitema/db/DBUserController.java
+++ ECommerce/src/com/hitema/db/DBUserController.java
@@ -1,5 +1,10 @@
 package com.hitema.db;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class DBUserController {
 	
 	private DBController db ;
@@ -15,7 +20,7 @@ public class DBUserController {
 	}
 	
 	public String Connexion (String pseudo , String motdepasse){
-		
+		try {
 		String sql = "SELECT nom , prenom , id_historique , id_user , montant FROM user WHERE pseudo=? AND mdp=?";
 		Connection connection = db.getConnection();
         PreparedStatement ps = connection.prepareStatement(sql);
@@ -24,6 +29,8 @@ public class DBUserController {
         ResultSet rs = ps.executeQuery();
 		Id_User=rs.getString("id_ser");
 		
+		
+		} catch (SQLException e) {}
 		if(Id_User!=""){
 			return Id_User;
 		}
@@ -33,41 +40,46 @@ public class DBUserController {
 	}
 	
 	public String AddUser(String pseudo,String mdp,String nom,String prenom){
-		
+		try {
 		String sql="SELECT id_user FROM user WHERE pseudo=?";
 		Connection connection = db.getConnection();
-		PrepareStatement ps=connection.prepareStatement(sql);
+		PreparedStatement ps=connection.prepareStatement(sql);
 		ps.setString(1,pseudo);
 		ResultSet rs = ps.executeQuery();
-		if(rs.pseudo!=""){
-			String sql="INSERT INTO User(pseudo,mdp,nom,prénom,montant) VALUES(?,?,?,?,5000);";
-			Connection connection=db.getConnection();
-			PrepareStatement ps=connection.prepareStatement(sql);
-			ps.setString(1,pseudo);
-			ps.setString(2,mdp);
-			ps.setString(3,nom);
-			ps.setString(4,prenom);
-			ps.executeUpdate();
+		if(rs.getString(pseudo)!=""){
+			String sql1="INSERT INTO User(pseudo,mdp,nom,prénom,montant) VALUES(?,?,?,?,5000);";
+			Connection connection1=db.getConnection();
+			PreparedStatement ps1=connection1.prepareStatement(sql1);
+			ps1.setString(1,pseudo);
+			ps1.setString(2,mdp);
+			ps1.setString(3,nom);
+			ps1.setString(4,prenom);
+			ps1.executeUpdate();
 			return "Ajout dans la base OK";
 		}
 		else{
 			return "Erreur , le pseudo existe deja !";
 		}
+		}
+		catch (SQLException e) {}
 		
-		
-		return "Test";
-
+		return null;
 	}
 	
 	public String[] GetInfosUser(String id_user){
-		
+		try{
 		String sql = "SELECT nom , prenom , id_historique , id_user , montant FROM user WHERE mdp=1";
 		Connection connection = db.getConnection();
-		PrepareStatement ps=connection.prepareStatement(sql);
+		PreparedStatement ps=connection.prepareStatement(sql);
 		ps.setString(1,id_user);
 		ResultSet rs=ps.executeQuery();
 		String tableau[]={rs.getString("nom"),rs.getString("prenom"),rs.getString("id_historique"),rs.getString("id_user"),rs.getString("montant")};
 		return tableau;
+		}
+		catch (SQLException e) {}
+		
+		return null;
+		
 	}
 	
 	public void close(){
